980db1c8ac70b7d0c080d415a596c24e7dc51f4c
SAJID ALI <135090212+sajidali23@users.noreply.github.com>
2024-10-02 03:29:38 +0000

    commit

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "./Navbar";
 import { useParams } from "react-router-dom";
-import { albumsData, assets,songsData } from "../assets/assets";
-import {PlayerContext} from '../context/PlayerContext';
+import { albumsData, assets, songsData } from "../assets/assets";
+import { PlayerContext } from "../context/PlayerContext";
 
 const DisplayAlbum = () => {
   const { id } = useParams();
-  const {playWithId} = useContext(PlayerContext)
+  const { playWithId } = useContext(PlayerContext);
   const albumData = albumsData[id];
 
   return (
@@ -22,37 +22,38 @@ const DisplayAlbum = () => {
           <h4>{albumData.desc}</h4>
           <p className="mt-1">
             <img src={assets.spotify_logo} className="inline-block w-5" />
-            <b className="ml-2">Spotify</b>* 12,345,678 likes * <b className="mr-2">50 songs</b>
+            <b className="ml-2">Spotify</b>* 12,345,678 likes *{" "}
+            <b className="mr-2">50 songs</b>
             about 2 hr 30 min
           </p>
         </div>
       </div>
       <div className="grid grid-cols-3 sm:grid-cols-4 mt-10 mb-l-2 text-[#a7a7a7]">
         <p>
-            <b className="mr-4">#</b>
-            Title
-         </p>   
+          <b className="mr-4">#</b>
+          Title
+        </p>
         <p>Album</p>
         <p className="hidden sm:block">Date Added</p>
         <img src={assets.clock_icon} className="m-auto w-4" />
       </div>
       <hr />
-      {
-        songsData.map((item,index)=>(
-          <div onClick={()=>playWithId(item.id)} key={index} className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer">
-              <p className="text-white">
-                <b className="mr-4 text-[#a7a7a7]">{index+1}</b>
-                <img src={item.image} className="inline w-10 mr-5"/>
-                {
-                  item.name
-                }
-              </p>
-              <p className="text-[15px]">{albumData.name}</p>
-              <p className="text-[15px] hidden sm:block">5 days to ago</p>
-              <p className="text-[15px]text-center">{item.duration}</p>
-          </div>
-        ))
-      }
+      {songsData.map((item, index) => (
+        <div
+          onClick={() => playWithId(item.id)}
+          key={index}
+          className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"
+        >
+          <p className="text-white">
+            <b className="mr-4 text-[#a7a7a7]">{index + 1}</b>
+            <img src={item.image} className="inline w-10 mr-5" />
+            {item.name}
+          </p>
+          <p className="text-[15px]">{albumData.name}</p>
+          <p className="text-[15px] hidden sm:block">5 days to ago</p>
+          <p className="text-[15px]text-center">{item.duration}</p>
+        </div>
+      ))}
     </>
   );
 };
